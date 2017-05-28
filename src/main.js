d334a49c7f6c94afc24682ca796936016a47a392
Christopher McDonald <mail@christophermcdonald.me>
2017-05-28 01:39:43 -0400

    react app setup

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Landing from './Landing';
+ 
+document.addEventListener('DOMContentLoaded', function() {
+  ReactDOM.render(
+    <Landing />,
+    document.getElementById('app')
+  );
+});
