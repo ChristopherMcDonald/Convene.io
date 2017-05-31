46f9970bf587f8c771e52cfad61f75fef3e90b58
Christopher McDonald <mail@christophermcdonald.me>
2017-05-31 07:47:52 -0400

    File restructure

--- app/src/Landing.js
+++ app/src/Landing.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './Landing.css';
 
-class App extends Component {
+class Landing extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
@@ -18,4 +16,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Landing;
--- app/src/index.js
+++ app/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Landing from './Landing';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Landing />, document.getElementById('root'));
 registerServiceWorker();
