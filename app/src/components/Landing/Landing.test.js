20d79132c86084e08b0241169f6db8b48d27313b
Christopher McDonald <mail@christophermcdonald.me>
2017-06-04 17:41:05 -0400

    WIP added some changes

--- app/src/components/Landing/Landing.test.js
+++ app/src/components/Landing/Landing.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Landing from './Landing';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Landing />, div);
 });
