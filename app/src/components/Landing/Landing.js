325f1a095bff9830c06ab8a0b059d5f259c437c3
Christopher McDonald <mail@christophermcdonald.me>
2017-07-08 22:22:30 -0400

    Fixing linter errors

--- app/src/components/Landing/Landing.js
+++ app/src/components/Landing/Landing.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 import NavBar from '../NavBar/NavBar';
 import Login from '../Login/Login';
 import Features from '../Features/Features';
-import About from '../About/About';
-import Contact from '../Contact/Contact';
+// import About from '../About/About';
+// import Contact from '../Contact/Contact';
 import './Landing.css';
 
 class Landing extends Component {
