e4fee7b54b6fb85a0a013e108c162ba2f535f0d2
Christopher McDonald <mail@christophermcdonald.me>
2017-08-06 16:47:03 -0400

    switch to promises and added statuses to endpoints

--- app/server.js
+++ app/server.js
@@ -2,6 +2,7 @@ var path = require('path');
 var config = require(path.resolve( __dirname, 'config.json'));
 var express = require('express');
 var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 var bodyParser = require('body-parser');
 var scrypt = require("scrypt");
 var scryptParameters = scrypt.paramsSync(0.1);
@@ -16,12 +17,12 @@ jwt.setSecret(config.secret);
 
 var app = express();
 app.use(bodyParser.json());
-app.use(function(req,res,next) {
+app.use((req,res,next) => {
     req.jwt_auth = false;
     if(req.headers['authorization'] &&
     req.headers['authorization'].startsWith('JWT')){
         var jwt_token = req.headers['authorization'].substr(4);
-        jwt.verify(jwt_token, function(err, jwt_data) {
+        jwt.verify(jwt_token, (err, jwt_data) => {
             if(err) throw err;
             req.jwt_auth = jwt_data;
             next();
@@ -33,18 +34,21 @@ app.use(function(req,res,next) {
 
 //
 var Users = require(path.resolve( __dirname, 'schemas/Users'));
-app.get('/users/me', function(req, res) {
+app.get('/users/me', (req, res) => {
     if(!req.jwt_auth) {
         res.send("error! no jwt_auth");
     }
-    Users.findOne({_id: req.jwt_auth.claims.claim}, 'email alias team', (err, user) => {
-        if(err) res.status(404).send("internal error!");
-        
+
+    Users.findOne({_id: req.jwt_auth.claims.claim}, 'email alias team').then((user) => {
         if(user) res.status(200).send({user: user});
         else res.status(404).send("couldn't find user")
+    }).catch((err) => {
+        res.status(500).send("internal error!");
     });
 });
 
+
+// TODO add some regex and validate dem fields
 app.post("/users/create", (req, res) => {
     var user = new Users({
         name: {
@@ -57,31 +61,28 @@ app.post("/users/create", (req, res) => {
         password: scrypt.kdfSync(req.body.password, scryptParameters)
     });
 
-    user.save((err, user) => {
-        if(err) {
-            console.log(err);
-        } else {
-            console.log("User made with id: " + user.id);
-            res.send(user.id);
-        }
+    user.save().then((user) => {
+        console.log("User made with id: " + user.id);
+        res.status(200).send(user.id);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).send("internal error");
     });
 });
 
 app.post("/users/login/", (req, res) => {
-    Users.findOne( {email: req.body.email}, (err, user) => {
-        if(err) {
-            Console.log(err);
-            res.send({id: -1});
+    Users.findOne( {email: req.body.email}).then((user) => {
+        if(scrypt.verifyKdfSync(user.password, req.body.password)) {
+            jwt.sign({claim: user.id},(err, data) => {
+                // the token must be added in the header as 'Authorization'
+                res.json({res: "valid", token: 'JWT ' + data});
+            });
         } else {
-            if(scrypt.verifyKdfSync(user.password, req.body.password)) {
-                jwt.sign({claim: user.id},(err, data) => {
-                    // the token must be added in the header as 'Authorization'
-                    res.json({res: "valid", token: 'JWT ' + data});
-                });
-            } else {
-                res.json({res: "invalid"});
-            }
+            res.json({res: "invalid"});
         }
+    }).catch((err) => {
+        Console.log(err);
+        res.status(500).send("internal error!");
     });
 });
 
